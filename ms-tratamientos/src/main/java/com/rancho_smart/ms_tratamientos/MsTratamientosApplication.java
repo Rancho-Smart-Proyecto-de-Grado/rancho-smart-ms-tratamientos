6c674a1ff597671a4a02d399ae14918113b9029f
el-sebas-galeano <jsebastian-galeano@javeriana.edu.co>
2024-10-12 02:49:03 -0500

    :sparkles: feat: Creación de la Aplicación SpringBoot.

---
+++ ms-tratamientos/src/main/java/com/rancho_smart/ms_tratamientos/MsTratamientosApplication.java
@@ -0,0 +1,13 @@
+package com.rancho_smart.ms_tratamientos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTratamientosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsTratamientosApplication.class, args);
+	}
+
+}
